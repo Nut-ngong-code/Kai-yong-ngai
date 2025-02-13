5794e9b1fcdc5941f51eeda6b07e21604e7f0b3a
new_nut <nuttum16@gmail.com>
2025-02-13 14:14:07 +0700

    renama s

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+async function loadComponent(url, targetid) {
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    document.getElementById(targetid).innerHTML = html;
+  } catch (error) {
+    console.error(`Error loading ${url}:`, error);
+  }
+  
+  const menuButton = document.querySelector('button[class*="md:hidden"]');
+  const mobileMenu = document.querySelector('div[class*="hidden md:hidden"]');
+  menuButton.addEventListener("click", () => {
+    const isHidden = mobileMenu.classList.contains("hidden");
+    if (isHidden) {
+      mobileMenu.classList.remove("hidden");
+    } else {
+      mobileMenu.classList.add("hidden");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("components/navbar/index.html", "navbar");
+  // loadComponent('components/navbar/index.html','navbar');
+  // loadComponent('components/navbar/index.html','navbar');
+});
+
